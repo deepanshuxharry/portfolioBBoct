43dbdbd11654fce701e6ca266c0fdcac717792d8
deepanshu sharma <deepanshu43sharma@gmail.com>
2024-10-14 19:08:04 +0530

    intial commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Card } from "./Card";
+
+export const Cards = () => {
+  const ecom =
+    " Developed a frontend app using React.js and Tailwind CSS to display products fetched from the API.Implemented product addition with local storage, viewing details, deleting items, and pagination.";
+  const tic =
+    " Built a Tic-Tac-Toe game in Java, reinforcing OOP and user input handling skills.Demonstrated proficiency in dynamic memory allocation, efficient array usage, and effective error handling.";
+  const music =
+    "Developed a dynamic music player web app using JavaScript, HTML, and CSS, showcasing proficiency in front-end web development.";
+
+  return (
+    <>
+      <div className="flex flex-row justify-center flex-wrap">
+        <Card
+          image={"./images/ecom2.png"}
+          heading={"E-Commerce-Product-Manager"}
+          link={"https://e-commerce-product-manager.vercel.app/"}
+          text={ecom}
+        ></Card>
+        <Card
+          image={"./images/tictactoe.png"}
+          link={"https://github.com/srishtii10/Tic-tac-toe"}
+          heading={"Tic-Tac-Toe"}
+          text={tic}
+        ></Card>
+        <Card
+          image={"./images/music.png"}
+          link={"https://srishtii10.github.io/Music-player-app/"}
+          heading={"Music-Player-App"}
+          text={music}
+        ></Card>
+      </div>
+    </>
+  );
+};
